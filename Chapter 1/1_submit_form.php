572a678d23cf77c5e52bb85069c3292ace536794
Anjali <112811128+anjali3123@users.noreply.github.com>
2022-09-17 12:38:10 +0530

    Form

---
+++ Chapter 1/1_submit_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>submit</title>
+</head>
+<body>
+    <form  action="submit.php" method="post">
+    Name:  <input type="text" name="name"><br><br>
+    Email: <input type="email" name="email"><br><br>
+    <input type="submit" name="submit" value="Submit">
+    <input type="reset" name="reset" value="Reset">
+    </form>
+        
+</body>
+</html>
